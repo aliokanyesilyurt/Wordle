748a67a17fc17e626db3828df0783c34ccf242e8
Ali Okan Yeşilyurt <okan.yesilyurt@std.yildiz.edu.tr>
2026-02-18 18:29:13 +0300

    functions.h dosyasına int kazanmadurumu fonksiyonu eklenerek bildirildi.

--- functions.h
+++ functions.h
@@ -6,3 +6,4 @@ void menu(); // Fonksiyonlar bildirildi.
 void kurallar();
 void kontrol(char *tahmin, const char *kelime, char *sondurum, char *yeriyanlis, char *tamyanlis);
 void durumyazdir(char *tahmin, const char *kelime, char *sondurum, char *yeriyanlis, char *tamyanlis);
+int kazanmadurumu(int kazanmadurum, const char *kelime, char *tahmin, int denemesayisi);
