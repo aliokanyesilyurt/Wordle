5b6b6ad5ef909a6ffe3dd768a849d80a92f6a1c9
Ali Okan Yeşilyurt <okan.yesilyurt@std.yildiz.edu.tr>
2026-02-18 17:57:58 +0300

    header.h dosyası açıldı ve tanımlamalar yapıldı.

---
+++ header.h
@@ -0,0 +1,18 @@
+#include <stdio.h> // Standart veri giriş-çıkış kütüphanesi.
+#include <stdlib.h> // Pause komutu için kullanacağım kütüphane.
+#include <string.h> // Kelime kontrolü için karakter dizisi kütüphanesi.
+#include <ctype.h> // Toupper yapmak için kullanacağım kütüphane.
+
+#define MAX_UZ 5 // Define tanımlamaları yapıldı.
+#define MAX_DENEME 6
+
+
+// Kütüphaneleri ve header dosyasını tanımla.
+// Global değişkenleri tanımla.
+// Main fonksiyonda kullanılacak değişkenleri tanımla.
+// Menü ekranını açtır. (1- oyuna başla 2- nasıl oynanır? 3- exit)
+// Oyuna başlarsa döngüye gir ve kelimeleri iste.
+// Kelimelerin durumunu fonksiyonların içinde kontrol et.,
+// Kazanma durumunu yazdır veya oyuna devam et.
+// Kazanma veya kaybetme durumu olursa yazdır.
+// Menüye dön.
