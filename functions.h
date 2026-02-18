e74dd4be39160ea7098f0800ec9500c8cc4cbae5
Ali Okan Yeşilyurt <okan.yesilyurt@std.yildiz.edu.tr>
2026-02-18 19:22:14 +0300

    Kelime havuzu eklemesi sonrası son hali.
    Global değişken tek yerde kalması için main içine atıldı.

--- functions.h
+++ functions.h
@@ -1,7 +1,5 @@
 #include "header.h" // Header buraya da tanımlandı.
 
-const char kelime[6]="ALTAN"; // Global değişken tanımlandı.
-
 void menu(); // Fonksiyonlar bildirildi.
 void kurallar();
 void kontrol(char *tahmin, const char *kelime, char *sondurum, char *yeriyanlis, char *tamyanlis);
